95c8a1a929115551bb3f7af2009c388bc607ed7b
russ9707 <Russell.Nwokolo77@gmail.com>
2026-02-23 22:44:30 -0600

    Implement serial decoding for motor control
    Added serial decoding function to read motor angles and distance.

--- Motor_control_V2.ino
+++ Motor_control_V2.ino
@@ -3,9 +3,11 @@
 
 // OpenCV updates these
 volatile float motor_x_angle = 0;  // degrees from center
-volatile float motor_y_angle = 0;  
+volatile float motor_y_angle = 0;
+volatile float distance = 0;
 volatile bool target_detected = false; //volatile type since it will be changing
 
+String data = "";
 // Motor pins
 const int dirPinx = 2;
 const int stepPinx = 4;
@@ -21,6 +23,29 @@ const int STEPS_PER_REV = MOTOR_STEPS * MICROSTEPS;
 float current_x_angle = 0;
 float current_y_angle = 0;
 
+void serial_decode(){
+  if (Serial.available()) {
+    data = Serial.readStringUntil('\n');
+
+    int firstComma = data.indexOf(',');
+    int secondComma = data.indexOf(',', firstComma + 1);
+    int thirdComma = data.indexOf(',', secondComma + 1);
+    if (firstComma > 0 && //just incase the parsing doesnt notice all of the commas
+      secondComma > firstComma && 
+      thirdComma > secondComma) {
+  
+      motor_x_angle = data.substring(0, firstComma).toInt();
+      motor_y_angle = data.substring(firstComma + 1, secondComma).toInt();
+      distance = data.substring(secondComma + 1, thirdComma).toInt();
+      target_detected = data.substring(thirdComma + 1).toInt();
+      Serial.println(target_detected);
+      Serial.println(motor_x_angle);
+      Serial.println(motor_y_angle);
+      Serial.println(distance);
+  }
+    
+  }
+}
 // Step conversion
 long degreesToSteps(float degrees){
   return (long)(degrees * STEPS_PER_REV / 360.0);
@@ -36,6 +61,7 @@ void stepMotorOnce(int dirPin, int stepPin, bool dir){
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(dirPinx, OUTPUT);
   pinMode(stepPinx, OUTPUT);
   pinMode(dirPiny, OUTPUT);
@@ -43,6 +69,7 @@ void setup() {
 }
 
 void loop() {
+  serial_decode();
   // Compute incremental movement
   float target_x = motor_x_angle;
   float target_y = motor_y_angle;
